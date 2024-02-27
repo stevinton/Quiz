e4ea543352c755470cd92895ffc35c468d7eccef
stevinton <stevinton9@gmail.com>
2024-02-27 15:52:02 +0530

    after editing

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-document.getElementById("options").addEventListener("click", function(){
-    console
-})
+'use strict';
+
+const answer = ['c) Hong Kong', 'b) Rome', 'a) Spanish', 'b) Rockstar Games', 'd) 1964']
+
+var option = function(ques, opt){
+    let op = document.querySelector(`label[for="${ques}-option-${opt}"]`);
+    verifyOption(op.textContent, op);
+}
+
+const verifyOption = function(ans, op){
+    if (answer.includes(ans)){
+        verified(op);
+    }else{
+        notVerified(op);
+    }
+}
+
+const verified = function(op){
+    op.classList.toggle("verfied");
+}
+
+const notVerified = function(op){
+    op.classList.toggle("notVerified");
+}
